1edf3a720846d12a358e28c20160b9438e97f9de
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-06-15 00:34:23 -0300

    initial

---
+++ Patches/InventoryPatch.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using HarmonyLib;
+using ProjectM;
+using ProjectM.Network;
+using ScarletCore.Data;
+using ScarletCore.Services;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace ScarletCarrier.Patches;
+
+
+[HarmonyPatch]
+internal static class EquipmentTransferSystemPatch {
+  private static readonly Database Database = Plugin.Database;
+  [HarmonyPatch(typeof(ReactToInventoryChangedSystem), nameof(ReactToInventoryChangedSystem.OnUpdate))]
+  [HarmonyPrefix]
+  public static void Prefix(EquipServantItemFromInventorySystem __instance) {
+    var query = __instance.__query_1850506269_0.ToComponentDataArray<InventoryChangedEvent>(Allocator.Temp);
+    foreach (var e in query) {
+      if (e.InventoryEntity.Equals(Entity.Null) || !e.InventoryEntity.Has<Attach>()) continue;
+
+      var servant = e.InventoryEntity.Read<Attach>().Parent;
+
+      if (e.ChangeType == InventoryChangedEventType.Moved || !servant.Has<ServantData>() || !servant.Has<Follower>()) continue;
+
+      var playerOwner = servant.Read<Follower>().Followed._Value;
+      var inventory = InventoryService.GetInventoryItems(servant);
+      var inventoryItems = new Dictionary<int, int>();
+      var platformId = playerOwner.Read<User>().PlatformId.ToString();
+
+      foreach (var item in inventory) {
+        if (item.ItemType.GuidHash == 0) continue;
+
+        if (inventoryItems.ContainsKey(item.ItemType.GuidHash)) {
+          inventoryItems[item.ItemType.GuidHash] += item.Amount;
+        } else {
+          inventoryItems[item.ItemType.GuidHash] = item.Amount;
+        }
+      }
+
+      Database.Save(platformId, inventoryItems);
+    }
+  }
+}
---
+++ Plugin.cs
@@ -0,0 +1,102 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using ScarletCore.Data;
+using VampireCommandFramework;
+using ScarletRCON.Shared;
+using ScarletCore.Systems;
+using ProjectM;
+using Unity.Entities;
+using ScarletCore.Services;
+using ScarletCore.Events;
+
+namespace ScarletCarrier;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("markvaaz.ScarletCore")]
+[BepInDependency("gg.deca.VampireCommandFramework")]
+[BepInDependency("markvaaz.ScarletRCON")]
+public class Plugin : BasePlugin {
+  static Harmony _harmony;
+  public static Harmony Harmony => _harmony;
+  public static Plugin Instance { get; private set; }
+  public static ManualLogSource LogInstance { get; private set; }
+  public static Settings Settings { get; private set; }
+  public static Database Database { get; private set; }
+
+  public override void Load() {
+    Instance = this;
+    LogInstance = Log;
+
+    Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+    _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+    _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+    EventManager.OnInitialize += (_, _) => {
+      ClearServants();
+    };
+
+    Settings = new Settings(MyPluginInfo.PLUGIN_GUID, Instance);
+    Database = new Database(MyPluginInfo.PLUGIN_GUID);
+    CommandRegistry.RegisterAll();
+    RconCommandRegistrar.RegisterAll();
+  }
+
+  public override bool Unload() {
+    _harmony?.UnpatchSelf();
+    TestCommands.Unload();
+    CommandRegistry.UnregisterAssembly();
+    RconCommandRegistrar.UnregisterAssembly();
+    return true;
+  }
+
+  public static void ReloadSettings() {
+    Settings.Dispose();
+    LoadSettings();
+  }
+  public static void LoadSettings() {
+    Settings.Section("General")
+      .Add("Enable", true, "Enable or disable the plugin");
+  }
+
+  public static void ClearServants() {
+    var servants = GameSystems.EntityManager.CreateEntityQuery(
+      ComponentType.ReadOnly<ServantData>(),
+      ComponentType.ReadOnly<Follower>()
+    );
+
+    foreach (var servant in servants.ToEntityArray(Unity.Collections.Allocator.Temp)) {
+      servant.Remove<Follower>();
+      InventoryService.ClearInventory(servant);
+      servant.Destroy();
+    }
+  }
+
+  /*
+    [CommandGroup("groupname")]
+    public class CommandGroup
+    {
+      [Command("commandname", "Description of the command")]
+      public static void CommandName(CommandContext context)
+      {
+        // Command implementation
+        context.Reply("Command executed successfully!");
+      }
+    }
+  */
+
+  /*
+    [RconCommandCategory("categoryname")]
+    public class RconCommandCategory
+    {
+      [RconCommand("commandname", "Description of the command")]
+      public static string CommandName()
+      {
+        // Command implementation
+        return "Command executed successfully!"
+      }
+    }
+  */
+}
