128b7848b5eb9032a167f6341148b36c8299fbcf
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-08-04 19:30:30 -0300

    Enhance Carrier Commands: add move and forced dismiss functionality, streamline summon and dismiss commands

--- Commands/Commands.cs
+++ Commands/Commands.cs
@@ -3,36 +3,30 @@ using ScarletCarrier.Services;
 using VampireCommandFramework;
 using ScarletCore.Utils;
 using System.Collections.Generic;
+using ScarletCore;
+using ProjectM;
+using ScarletCarrier.Models;
+using ScarletCore.Systems;
+using ScarletCore.Data;
 
 namespace ScarletCarrier.Commands;
 
 [CommandGroup("carrier", "car")]
 public static class CarrierCommands {
+  public static Dictionary<PlayerData, ActionId> _selectedActions = [];
+
   [Command("call", shortHand: "c")]
   public static void SummonCommand(ChatCommandContext ctx) {
     var platformId = ctx.User.PlatformId;
 
-    if (CarrierService.HasServant(platformId)) {
-      CarrierService.TeleportToPlayer(platformId);
-      ctx.Reply($"Your ~carrier~ has been summoned!".Format());
-      return;
-    }
-
     CarrierService.Spawn(platformId);
 
     ctx.Reply($"Your ~carrier~ has been summoned!".Format());
-    ctx.Reply($"Use ~.carrier dismiss~ to dismiss it early.".Format());
-    ctx.Reply($"Do ~NOT~ store equipment in the carrier, it will be lost.".FormatError());
   }
 
   [Command("dismiss", shortHand: "d")]
   public static void DismissCommand(ChatCommandContext ctx) {
     var platformId = ctx.User.PlatformId;
-
-    if (!CarrierService.HasServant(platformId)) {
-      ctx.Reply($"You do not have a ~carrier~ summoned.".FormatError());
-      return;
-    }
 
     CarrierService.Dismiss(platformId);
 
@@ -134,6 +128,105 @@ public static class CarrierCommands {
 
     Plugin.Database.Save(CarrierService.CustomAppearances, appearanceData);
 
+    var carrier = CarrierService.GetCarrier(playerData.PlatformId);
+
+    carrier.SwapServantAppearance();
+
     ctx.Reply($"Your carrier's appearance has been changed to ~{names[number]}~.".Format());
+  }
+
+  [Command("move", shortHand: "m", adminOnly: true)]
+  public static void MoveCommand(ChatCommandContext ctx) {
+    if (!PlayerService.TryGetById(ctx.User.PlatformId, out var playerData)) {
+      ctx.Reply($"Error: Player ~{ctx.User.CharacterName}~ not found.".Format());
+      return;
+    }
+
+    if (_selectedActions.ContainsKey(playerData)) {
+      ctx.Reply($"You are already moving a carrier.".FormatError());
+      return;
+    }
+
+    var hoveredEntity = playerData.CharacterEntity.Read<EntityInput>().HoveredEntity;
+    if (!hoveredEntity.Exists()) {
+      ctx.Reply($"Please aim at the carrier you want to move.".FormatError());
+      return;
+    }
+
+    if (!hoveredEntity.Has<ServantData>() || !hoveredEntity.Has<NameableInteractable>() || hoveredEntity.Read<NameableInteractable>().Name.Value != Carrier.Id) {
+      ctx.Reply($"The hovered entity is not a carrier.".FormatError());
+      return;
+    }
+
+    _selectedActions[playerData] = ActionScheduler.OncePerFrame((end) => {
+      var inp = playerData.CharacterEntity.Read<EntityInput>();
+
+      if (inp.State.InputsDown == SyncedButtonInputAction.Primary) {
+        end();
+        _selectedActions.Remove(playerData);
+        ctx.Reply("Carrier moved.".FormatSuccess());
+        return;
+      }
+
+      hoveredEntity.SetPosition(inp.AimPosition);
+    });
+
+    ActionScheduler.Delayed(() => {
+      if (!_selectedActions.ContainsKey(playerData)) return;
+      ActionScheduler.CancelAction(_selectedActions[playerData]);
+    }, 180);
+
+    ctx.Reply($"You are now moving the carrier. ~Click to place it~.".Format());
+  }
+
+  [Command("forcedismiss", shortHand: "fd", adminOnly: true)]
+  public static void ForceDismissCommand(ChatCommandContext ctx) {
+    if (!PlayerService.TryGetById(ctx.User.PlatformId, out var playerData)) {
+      ctx.Reply($"Error: Player ~{ctx.User.CharacterName}~ not found.".Format());
+      return;
+    }
+
+    var hoveredEntity = playerData.CharacterEntity.Read<EntityInput>().HoveredEntity;
+    if (!hoveredEntity.Exists()) {
+      ctx.Reply($"Please aim at the carrier you want to move.".FormatError());
+      return;
+    }
+
+    if (!hoveredEntity.Has<ServantData>() || !hoveredEntity.Has<NameableInteractable>() || hoveredEntity.Read<NameableInteractable>().Name.Value != Carrier.Id || !hoveredEntity.Has<EntityOwner>()) {
+      ctx.Reply($"The hovered entity is not a carrier.".FormatError());
+      return;
+    }
+
+    var owner = hoveredEntity.Read<EntityOwner>().Owner;
+    var player = owner.GetPlayerData();
+    var carrier = CarrierService.GetCarrier(player.PlatformId);
+
+    if (carrier == null) {
+      ctx.Reply($"Something went wrong, the carrier for player {player.Name} was not found.".FormatError());
+      return;
+    }
+
+    carrier.Dismiss();
+
+    ctx.Reply($"Carrier for player ~{player.Name}~ has been dismissed.".FormatSuccess());
+  }
+
+  [Command("forcedismiss", shortHand: "fd", adminOnly: true)]
+  public static void ForceDismissCommand(ChatCommandContext ctx, string playerName) {
+    if (!PlayerService.TryGetByName(playerName, out var playerData)) {
+      ctx.Reply($"Error: Player ~{playerName}~ not found.".Format());
+      return;
+    }
+
+    var carrier = CarrierService.GetCarrier(playerData.PlatformId);
+
+    if (carrier == null) {
+      ctx.Reply($"Carrier for player ~{playerData.Name}~ not found.".FormatError());
+      return;
+    }
+
+    carrier.Dismiss();
+
+    ctx.Reply($"Carrier for player ~{playerData.Name}~ has been dismissed.".FormatSuccess());
   }
 }
